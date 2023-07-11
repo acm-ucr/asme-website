19d126461ebe7888626933ae19d72c0c85e2d82a
Suhani Chaudhary <suhanichaudhary@Suhanis-MacBook-Pro-2.local>
2023-07-11 16:24:20 -0700

    Using tailwind pl utility class

--- src/components/ProjectsTab.jsx
+++ src/components/ProjectsTab.jsx
@@ -8,10 +8,7 @@ const ProjectsTab = () => {
       <div className="flex flex-col w-64"></div>
       <Image src={bot} className="w-60 h-52" />
       <div className="flex flex-col" style={{ width: "680px" }}>
-        <text
-          className="text-2xl flex-wrap font-poppins font-bold text-asme-blue-500"
-          style={{ paddingLeft: "20px" }}
-        >
+        <text className="text-2xl flex-wrap font-poppins font-bold text-asme-blue-500 pl-5">
           BattleBots
         </text>
         <p
