39f530261c1c2ae309d65bb12e459d7400a1e380
Suhani Chaudhary <suhanichaudhary@Suhanis-MacBook-Pro-2.local>
2023-07-11 16:28:13 -0700

    Revert "Using tailwind pl utility class"
    This reverts commit 449f130def2e7dc6c88c2eb515d7b995863d9547.

--- src/components/ProjectsTab.jsx
+++ src/components/ProjectsTab.jsx
@@ -8,7 +8,10 @@ const ProjectsTab = () => {
       <div className="flex flex-col w-64"></div>
       <Image src={bot} className="w-60 h-52" />
       <div className="flex flex-col" style={{ width: "680px" }}>
-        <text className="text-2xl flex-wrap font-poppins font-bold text-asme-blue-500 pl-5">
+        <text
+          className="text-2xl flex-wrap font-poppins font-bold text-asme-blue-500"
+          style={{ paddingLeft: "20px" }}
+        >
           BattleBots
         </text>
         <p
