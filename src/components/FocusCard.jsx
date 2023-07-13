a6e2254a17b695ef4c2d0f557858412c52a8cdbc
Vaneesha Singh <vsingh@Vaneeshas-MacBook-Pro.local>
2023-07-13 12:09:17 -0700

    Update style to conform to tailwind

--- src/components/FocusCard.jsx
+++ src/components/FocusCard.jsx
@@ -5,20 +5,14 @@ import { RiComputerLine } from "react-icons/ri";
 
 const FocusCard = () => {
   return (
-    <div
-      className="flex flex-wrap text-white"
-      style={{ justifyContent: "center" }}
-    >
-      <div
-        className="flex flex-col justify-between bg-asme-blue-200"
-        style={{ margin: "3.5%", width: "289px", height: "289px" }}
-      >
+    <div className="flex flex-wrap justify-center text-white">
+      <div className="flex flex-col justify-between m-[3.5%] w-[289px] h-[289px] bg-asme-blue-200">
         <div className="flex justify-between">
           <div className="ml-4 mt-1">
             <FaGears size={65} />
           </div>
 
-          <div className="underline decoration-4 mt-2 mr-4 text-2xl font-bold justify-end">
+          <div className="mt-2 mr-4 justify-end underline decoration-4 text-2xl font-bold">
             Technical
           </div>
         </div>
@@ -29,16 +23,13 @@ const FocusCard = () => {
         </div>
       </div>
 
-      <div
-        className="flex flex-col justify-between bg-asme-blue-400"
-        style={{ margin: "3.5%", width: "289px", height: "289px" }}
-      >
+      <div className="flex flex-col justify-between m-[3.5%] w-[289px] h-[289px] bg-asme-blue-400">
         <div className="flex justify-between">
           <div className="ml-4 mt-1">
             <GoPeople size={65} />
           </div>
 
-          <div className="underline decoration-4 mt-2 mr-4 text-2xl font-bold justify-end">
+          <div className="mt-2 mr-4 justify-end underline decoration-4 text-2xl font-bold">
             Social
           </div>
         </div>
@@ -49,16 +40,13 @@ const FocusCard = () => {
         </div>
       </div>
 
-      <div
-        className="flex flex-col justify-between bg-asme-blue-600"
-        style={{ margin: "3.5%", width: "289px", height: "289px" }}
-      >
+      <div className="flex flex-col justify-between m-[3.5%] w-[289px] h-[289px] bg-asme-blue-600">
         <div className="flex justify-between">
           <div className="ml-4 mt-1">
             <RiComputerLine size={65} />
           </div>
 
-          <div className="underline decoration-4 mt-2 mr-4 text-2xl font-bold">
+          <div className="mt-2 mr-4 justify-end underline decoration-4 text-2xl font-bold">
             Professional
           </div>
         </div>
