c12912880900e747e0ce75ba9182d6873436903a
Menthy <wumengxuan0728@gmail.com>
2023-07-14 12:25:15 -0700

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Header = () => {
   return (
     <div className="w-full flex relative">
-      <Image src={header} className="w-full" />
+      <Image src={header} className="w-full" alt="header" />
       <div className="absolute bg-asme-transparentBlue w-2/3 h-[22.7%] bottom-10 flex items-center">
         <p className="text-5xl text-white font-poppins font-bold mt-12 ml-8">
           Projects
