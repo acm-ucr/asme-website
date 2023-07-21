677bdb1460d28b3b593eacc5a702637a217fc07a
NovaSagittarii <novasagittarii@gmail.com>
2023-07-20 22:48:33 -0600

    always use textShadow color

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -5,7 +5,7 @@ const UpcomingEvents = () => {
     <div>
       <div className="font-poppins font-bold text-l md:text-3xl m-0 relative">
         Upcoming Events
-        <div className="absolute bottom-0 left-0 text-asme-blue-100 md:text-asme-textShadow z-[-1] scale-[1.8] md:scale-[2.4] origin-bottom-left translate-x-[-10%]">
+        <div className="absolute bottom-0 left-0 text-asme-textShadow z-[-1] scale-[1.8] md:scale-[2.4] origin-bottom-left translate-x-[-10%]">
           Upcoming Events
         </div>
       </div>
