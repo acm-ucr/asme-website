3a945595cea1a52b0b1e94e5673680c53d2c7b2d
Suhani Chaudhary <suhanichaudhary@Suhanis-MacBook-Pro-2.local>
2023-07-22 15:25:09 -0700

    Add project description to projects page

--- src/components/ProjectDescription.jsx
+++ src/components/ProjectDescription.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import Image from "next/image";
 
-const ProjectDescription = () => {
-  return <div>ProjectDescription</div>;
+const ProjectDescription = (props) => {
+  const name = props.name;
+  const description = props.description;
+  const picture1 = props.picture1;
+  const picture2 = props.picture2;
+
+  return (
+    <div className="pt-4 relative">
+      <div className="flex flex-row relative h-40">
+        <div className="bg-asme-blue-400 w-56 h-16 top-12 absolute"></div>
+        <text className="bg-asme-blue-600 text-white text-2xl font-poppins font-bold w-52 h-16 top-14 pl-16 pt-3 absolute">
+          {name}
+        </text>
+      </div>
+
+      <div>
+        <div className="flex flex-row font-abel justify-center">
+          <p className="w-[800px]">{description}</p>
+        </div>
+
+        <div className="flex flex-row justify-center">
+          <Image
+            className="w-[520px] h-[350px] p-3"
+            src={picture1}
+            alt="no image"
+          />
+          <Image
+            className="w-[520px] h-[350px] p-3"
+            src={picture2}
+            alt="no image"
+          />
+        </div>
+      </div>
+
+      <p
+        className="m-0 p-0 whitespace-nowrap font-bold font-poppins text-asme-textShadow text-6xl origin-top-left -rotate-90 pt-2"
+        style={{ textTransform: "uppercase" }}
+      >
+        {name}
+      </p>
+
+      <div className="flex flex-row justify-end">
+        <p
+          className="m-0 p-0 whitespace-nowrap font-bold font-poppins text-asme-textShadow text-6xl pt-2 justify-end rotate-90 -translate-y-full absolute origin-top-right top-[550px]"
+          style={{ textTransform: "uppercase" }}
+        >
+          {name}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectDescription;
