084bd8ccfd291cf42b1b5ef5b5d1c59968854e07
NovaSagittarii <novasagittarii@gmail.com>
2023-07-28 00:11:02 -0600

    improve focus card responsiveness

--- src/components/FocusCards.jsx
+++ src/components/FocusCards.jsx
@@ -26,7 +26,7 @@ const cardList = [
 ];
 const FocusCards = () => {
   return (
-    <Row className="w-2/3 my-12 font-poppins">
+    <Row className="p-4 w-full 2xl:p-0 2xl:w-2/3 my-12 font-poppins">
       {cardList.map((card, index) => (
         <Col xs={12} md={4} className="p-3" key={index}>
           <FocusCard
