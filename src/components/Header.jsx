ece31b3e19e35a78c667f4139ae0a8934b629adc
NovaSagittarii <novasagittarii@gmail.com>
2023-07-27 23:44:24 -0600

    improve header responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import header from "../../public/header.png";
+import header from "../../public/header.png"; // this image is a cropped version of the one on figma!!
 import Image from "next/image";
 const Header = ({ title }) => {
   return (
     <div className="w-full flex relative">
       <Image src={header} className="w-full" alt="header" />
-      <div className="absolute bg-asme-transparentBlue w-2/3 h-[22.7%] bottom-10 flex items-center">
-        <p className="text-5xl text-white font-poppins font-bold mt-12 ml-8">
+      <div className="absolute bg-asme-transparentBlue w-1/2 md:w-2/3 my-4 p-4 bottom-0 flex items-center">
+        <p className="text-2xl md:text-5xl text-white font-poppins font-bold">
           {title}
         </p>
       </div>
