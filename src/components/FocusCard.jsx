3a69681e17555d0a761e3895880eb293b3a4abea
NovaSagittarii <novasagittarii@gmail.com>
2023-07-28 00:11:02 -0600

    improve focus card responsiveness

--- src/components/FocusCard.jsx
+++ src/components/FocusCard.jsx
@@ -4,17 +4,17 @@ import React from "react";
 const FocusCard = ({ name, text, icon, bgColor }) => {
   return (
     <div
-      className={`flex flex-col justify-between aspect-square text-white ${bgColor} w-full p-3`}
+      className={`flex flex-col justify-between aspect-square text-white ${bgColor} w-full sm:p-3 md:p-1 lg:p-3`}
     >
       <div className="flex justify-between">
         <div className="ml-4 mt-1">{icon}</div>
 
-        <div className="mt-2 mr-4 justify-end underline decoration-4 text-2xl font-bold">
+        <div className="mt-2 mr-4 justify-end underline decoration-4 text-2xl md:text-lg lg:text-2xl font-bold">
           {name}
         </div>
       </div>
 
-      <div className="mb-3 ml-5 text-xl">{text}</div>
+      <div className="mb-3 ml-5 text-xl md:text-base lg:text-xl">{text}</div>
     </div>
   );
 };
