aff35c436341c8d7c488f8e289b555fa7855c931
menthy-wu <wumengxuan0728@gmail.com>
2023-08-11 00:51:49 -0700

    add not found

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -4,10 +4,12 @@ import ProjectTitle from "./ProjectTitle.jsx";
 
 const ProjectsSection = () => {
   return (
-    <div className="my-8 w-full overflow-hidden items-center flex flex-col">
+    <div className="my-8 w-full overflow-hidden items-center flex flex-col relative">
       <ProjectTitle />
-
       <ProjectsTab />
+      <p className="whitespace-nowrap font-bold font-poppins text-asme-textShadow rotate-90 text-6xl absolute right-5 origin-top-left translate-x-full">
+        PROJECTS
+      </p>
     </div>
   );
 };
