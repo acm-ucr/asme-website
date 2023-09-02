d457559bef1725eaef92d6b8f52586eb3602c368
Suhani Chaudhary <suhanichaudhary@Suhanis-MacBook-Pro-2.local>
2023-09-01 18:24:45 -0700

    Fix board image import statemtent

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "@/components/Header";
 import Board from "@/components/board/Board";
-import image from "public/Board.png";
+import image from "public/board.png";
 
 const board = () => {
   return (
