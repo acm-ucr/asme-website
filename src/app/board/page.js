d3cee85b71aa605f4f8439bb6d3b451bba09d109
Suhani Chaudhary <suhanichaudhary@Suhanis-MacBook-Pro-2.local>
2023-09-01 17:48:37 -0700

    Revert "Add board member card"
    This reverts commit d43a6676965b580e116e2cb2254302819dbd39e4.

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -1,22 +1,11 @@
 import React from "react";
 import Header from "@/components/Header";
-import Board from "@/components/Board";
-import image from "public/Board.png";
 
 const board = () => {
   return (
-    <div>
-      <div className="w-full">
-        <Header title="Board" />
-      </div>
-
-      <div>
-        <Board
-          image={image}
-          position="Position Title"
-          name="Firstname Lastname"
-        />
-      </div>
+    <div className="w-full">
+      <Header title="Board" />
+      board
     </div>
   );
 };
