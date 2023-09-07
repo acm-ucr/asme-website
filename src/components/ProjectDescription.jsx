a4e25e22eb344e4a1790c7c6d811c9ba53dfd829
menthy-wu <wumengxuan0728@gmail.com>
2023-09-07 12:41:21 -0700

    fix

--- src/components/ProjectDescription.jsx
+++ src/components/ProjectDescription.jsx
@@ -13,20 +13,20 @@ const ProjectDescription = (props) => {
         {name}
       </p>
 
-      <p className="self-center lg:text-base text-sm w-10/12 md:w-2/3">
+      <p className="self-center lg:text-base text-sm w-10/12 md:w-2/3relative z-10 ">
         {description}
       </p>
 
-      <div className="flex flex-col md:flex-row justify-center items-center w-10/12 self-center gap-3 m-3">
+      <div className="relative z-10 flex flex-col md:flex-row justify-center items-center w-10/12 self-center gap-3 m-3">
         <Image className="w-10/12 md:w-1/2" src={picture1} alt="no image" />
         <Image className="w-10/12 md:w-1/2" src={picture2} alt="no image" />
       </div>
 
-      <p className="m-0 p-0 whitespace-nowrap font-bold font-poppins text-asme-textShadow text-4xl lg:text-6xl origin-top-left -rotate-90 pt-2">
+      <p className="z-0 m-0 p-0 whitespace-nowrap font-bold font-poppins text-asme-textShadow text-4xl lg:text-6xl origin-top-left -rotate-90 pt-2">
         {name.toUpperCase()}
       </p>
 
-      <p className="m-0 p-0 whitespace-nowrap font-bold font-poppins text-asme-textShadow text-4xl lg:text-6xl pt-2 justify-end rotate-90 -translate-y-full absolute origin-top-right lg:top-[550px] top-80 right-0">
+      <p className="z-0 m-0 p-0 whitespace-nowrap font-bold font-poppins text-asme-textShadow text-4xl lg:text-6xl pt-2 justify-end rotate-90 -translate-y-full absolute origin-top-right lg:top-[550px] top-80 right-0">
         {name.toUpperCase()}
       </p>
     </div>
